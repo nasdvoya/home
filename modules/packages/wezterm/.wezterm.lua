1d63dde9d1930cc94691c7f9349fe6caee0b3c79
sasha <nasdvoya@proton.me>
2024-07-28 13:52:07 +0100

    Wezterm cleanup

--- modules/packages/wezterm/.wezterm.lua
+++ modules/packages/wezterm/.wezterm.lua
@@ -106,13 +106,7 @@ function readjust_font_size(window, pane)
   end
 end
 
--- Apply color scheme based on the WEZTERM_THEME environment variable
-local themes = {
-  nord = "Nord (Gogh)",
-  onedark = "One Dark (Gogh)",
-}
-local success, stdout, stderr = wezterm.run_child_process({ os.getenv("SHELL"), "-c", "printenv WEZTERM_THEME" })
 local selected_theme = stdout:gsub("%s+", "") -- Remove all whitespace characters including newline
-config.color_scheme = themes[selected_theme]
+config.color_scheme = "One Dark (Gogh)";
 
 return config
