70e4c24f54d7b3e0d09fcb805ea14d5a33b8d6d7
sasha <nasdvoya@proton.me>
2024-08-02 10:24:27 +0100

    Test unstable

--- flake.nix
+++ flake.nix
@@ -3,11 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }: 
+  outputs = inputs@{ nixpkgs, home-manager, nixpkgs-unstable, ... }: 
+    let 
+      system ="x86_64-linux";
+      lib = nixpkgs.lib;
+      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
+      username = "sasha";
+    in
   {
     nixosConfigurations = {
       sasha = nixpkgs.lib.nixosSystem {
@@ -19,7 +27,12 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.sasha = import ./hosts/sasha/home.nix;
-	    home-manager.extraSpecialArgs = { inherit inputs; };
+	    home-manager.extraSpecialArgs = { 
+              inherit inputs;
+              inherit username;
+              inherit pkgs-unstable;
+              inherit pkgs;
+            };
 
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
--- modules/packages/vscode.nix
+++ modules/packages/vscode.nix
@@ -1,7 +1,46 @@
-{ config, pkgs, lib, ... }:
+{ pkgs, ... }:
+
+let
+
+  dotnet-full =
+    with pkgs.dotnetCorePackages;
+    combinePackages [
+      sdk_8_0
+      runtime_8_0
+      aspnetcore_8_0
+    ];
+
+  deps = (
+    ps:
+    with ps;
+    [
+      rustup
+      zlib
+      openssl.dev
+      pkg-config
+      stdenv.cc
+      cmake
+      mono
+      msbuild
+    ]
+    ++ [ dotnet-full ]
+  );
+in
+
 {
   programs.vscode = {
-    enable = true; 
+    enable = true;
+    package =
+      (pkgs.vscode.overrideAttrs (prevAttrs: {
+        nativeBuildInputs = prevAttrs.nativeBuildInputs ++ [ pkgs.makeWrapper ];
+        postFixup =prevAttrs.postFixup
+          + ''
+            wrapProgram $out/bin/code \
+              --set DOTNET_ROOT "${dotnet-full}" \
+              --prefix PATH : "~/.dotnet/tools"
+          '';
+      })).fhsWithPackages
+        (ps: deps ps);
+    extensions = [ ];
   };
 }
-
