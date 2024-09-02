5f8a3bc22aff91fcdc1b401ed01e7506fd3653ec
sasha <nasdvoya@proton.me>
2024-09-02 11:29:19 +0100

    Change dev env script

--- scripts/development-environment.sh
+++ scripts/development-environment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 development-environment() {
-  # Check if a .sln or .csproj file exists in the current directory
-  if ls *.sln 1> /dev/null 2>&1 || ls *.csproj 1> /dev/null 2>&1; then
+  # Check if a .sln file exists in the current directory
+  if ls *.sln 1> /dev/null 2>&1 && [ ! -f flake.nix ]; then
     # Check if not already in a nix-shell environment and if the command hasn't been run yet
     if [ -z "$IN_NIX_SHELL" ] && [ -z "$DEV_ENV_RAN" ]; then
       # Run nix develop with the GitHub flake and set the DEV_ENV_RAN flag
