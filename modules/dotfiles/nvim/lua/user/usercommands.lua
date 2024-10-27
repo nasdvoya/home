854896b2332221a2d3ec06520818c95fdd1bfcdb
sasha <nasdvoya@proton.me>
2024-10-27 22:32:50 +0000

    Live server starts at the location of current buffer

--- modules/dotfiles/nvim/lua/user/usercommands.lua
+++ modules/dotfiles/nvim/lua/user/usercommands.lua
@@ -2,12 +2,13 @@ local M = {}
 
 function M.setup()
   vim.api.nvim_create_user_command("StartLiveServer", function()
-    vim.fn.jobstart("live-server --host=127.0.0.1 --port=8080 " .. vim.fn.getcwd(), {
+    local buffer_path = vim.fn.expand('%:p:h')
+    vim.fn.jobstart("live-server --host=127.0.0.1 --port=8080 " .. buffer_path, {
       on_stdout = function(_, data)
         print(table.concat(data, "\n"))
       end,
       on_stderr = function(_, data)
-        print("Error: " .. table.concat(data, "\n"))
+        print("Error starting live-server: " .. table.concat(data, "\n"))
       end,
     })
   end, {})
