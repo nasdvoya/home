8c9450e754bbe2da013336a48666f11c907e4606
sasha <nasdvoya@proton.me>
2024-10-29 21:14:02 +0000

    Wayland on

--- hosts/sasha/configuration.nix
+++ hosts/sasha/configuration.nix
@@ -59,7 +59,7 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
   # Activate the X11
-  services.xserver.displayManager.gdm.wayland = false;
+  # services.xserver.displayManager.gdm.wayland = false;
 
   # Configure keymap in X11
   services.xserver = {
