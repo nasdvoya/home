76770583fac1ee6608520f466de94860307209b8
sasha <nasdvoya@proton.me>
2024-11-04 23:38:49 +0000

    Postgre tcp setup

--- modules/services/postgresql.nix
+++ modules/services/postgresql.nix
@@ -4,9 +4,16 @@
   services.postgresql = {
     enable = true;
     ensureDatabases = [ "testdb" ];
+    enableTCPIP = true;
     authentication = pkgs.lib.mkOverride 10 ''
-      #type database  DBuser  auth-method
-      local all       all     trust
+      # Allow local connections via Unix socket
+      local   all   all                trust
+      
+      # Allow IPv4 connections with password authentication
+      host    all   all   127.0.0.1/32   scram-sha-256
+
+      # Allow IPv6 connections with password authentication
+      host    all   all   ::1/128        scram-sha-256
     '';
   };
 }
