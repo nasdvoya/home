005705cb43f3290d05f77b603824967fa01ae785
sasha <nasdvoya@proton.me>
2024-12-02 12:15:32 +0000

    Test cache

---
+++ hosts/sasha/broker.nix
@@ -0,0 +1,13 @@
+
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://broker.cachix.org"
+      ];
+      trusted-public-keys = [
+        "broker.cachix.org-1:oQR8t6zenpe+JdJXYDup+JPxMExJ0u8/ObZafkDm88k="
+      ];
+    };
+  };
+}
--- hosts/sasha/configuration.nix
+++ hosts/sasha/configuration.nix
@@ -5,6 +5,7 @@
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ../../modules/services
+    ./cachix.nix
   ];
 
   programs.nix-ld.enable = true;
