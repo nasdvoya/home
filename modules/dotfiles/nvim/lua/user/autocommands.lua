ef79de47dea39fd0d16a8eae19121db3df62a9c9
sasha <nasdvoya@proton.me>
2024-12-19 17:45:52 +0000

    Disable format on save

--- modules/dotfiles/nvim/lua/user/autocommands.lua
+++ modules/dotfiles/nvim/lua/user/autocommands.lua
@@ -1,17 +1,17 @@
 -- Autocommands
 
 -- Group for format on save
-local format_on_save_group = vim.api.nvim_create_augroup("format_on_save", { clear = true })
-vim.api.nvim_create_autocmd("BufWritePre", {
-  group = format_on_save_group,
-  pattern = "*",
-  desc = "Run LSP formatting on a file on save",
-  callback = function()
-    if vim.fn.exists(":Format") > 0 then
-      vim.cmd.Format()
-    end
-  end,
-})
+-- local format_on_save_group = vim.api.nvim_create_augroup("format_on_save", { clear = true })
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   group = format_on_save_group,
+--   pattern = "*",
+--   desc = "Run LSP formatting on a file on save",
+--   callback = function()
+--     if vim.fn.exists(":Format") > 0 then
+--       vim.cmd.Format()
+--     end
+--   end,
+-- })
 
 -- Group for highlight yank
 local highlight_yank_group = vim.api.nvim_create_augroup("highlight_yank", { clear = true })
