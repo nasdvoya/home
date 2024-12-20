7910bb1a3692ccfd4d48904a1b1bff6b9265d643
sasha <nasdvoya@proton.me>
2024-12-20 17:54:47 +0000

    Some git aliases

--- hosts/sasha/git.nix
+++ hosts/sasha/git.nix
@@ -1,7 +1,4 @@
 {
-  config,
-  pkgs,
-  lib,
   ...
 }:
 {
@@ -10,9 +7,17 @@
     enable = true;
     userName = "sasha";
     userEmail = "nasdvoya@proton.me";
+    aliases = {
+      l = "log --pretty=format:'%C(#cccc00)%h %Cred%ad %Creset%<(60,trunc)%s%C(auto)%d %C(magenta)%<(15,trunc)%an' --date=format:'%y%m%d'";
+      dblame = "blame -w -C";
+      b = "branch --format='%(color:#cccc00)%(objectname:short) %(color:red)%(committerdate:short) %(color:bold white)%(refname:short)'";
+    };
     extraConfig = {
       core = {
         sshCommand = "ssh -i ~/.ssh/nasdvoya";
+      };
+      branch = {
+        sort = "-committerdate";
       };
     };
     signing = {
