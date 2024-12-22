74babd01f20cb98cfe53840fc5e0a508700f917d
sasha <nasdvoya@proton.me>
2024-12-22 14:33:00 +0000

    Disable ai for Rust

--- modules/dotfiles/nvim/lua/plugins/supermaven.lua
+++ modules/dotfiles/nvim/lua/plugins/supermaven.lua
@@ -5,12 +5,17 @@ return {
     config = function()
       require("supermaven-nvim").setup({
         keymaps = {
-          accept_suggestion = "<C-CR>"
+          accept_suggestion = "<C-CR>",
+          accept_word = "<C-j>",
         },
+        ignore_filetypes = { rust = true },
         color = {
           suggestion_color = "#6d7f8b",
           cterm = 244,
         },
+        condition = function()
+          return false 
+        end,
       })
     end,
   },
