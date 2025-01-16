475eb1ce310f8defeb13c0b47f3705993e1cd2ac
sasha <nasdvoya@proton.me>
2025-01-16 14:38:58 +0000

    Supermaven keybind

--- modules/dotfiles/nvim/lua/config/plugins/supermaven.lua
+++ modules/dotfiles/nvim/lua/config/plugins/supermaven.lua
@@ -3,7 +3,7 @@ return {
   config = function()
     require("supermaven-nvim").setup({
       keymaps = {
-        accept_suggestion = "C-CR",
+        accept_suggestion = "<leader>y",
       },
     })
   end,
