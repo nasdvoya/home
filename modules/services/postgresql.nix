12ab868fabcf5b4e78ebeab58a72fdcc908a6098
sasha <nasdvoya@proton.me>
2025-01-26 20:07:47 +0000

    Test postgresql

--- modules/services/postgresql.nix
+++ modules/services/postgresql.nix
@@ -3,17 +3,17 @@
 {
   services.postgresql = {
     enable = true;
-    # ensureDatabases = [ "testdb" ];
-    # enableTCPIP = true;
-    # authentication = pkgs.lib.mkOverride 10 ''
-    #   # Allow local connections via Unix socket
-    #   local   all   all                trust
-    #
-    #   # Allow IPv4 connections with password authentication
-    #   host    all   all   127.0.0.1/32   scram-sha-256
-    #
-    #   # Allow IPv6 connections with password authentication
-    #   host    all   all   ::1/128        scram-sha-256
-    # '';
+    ensureDatabases = [ "testdb" ];
+    enableTCPIP = true;
+    authentication = pkgs.lib.mkOverride 10 ''
+      # Allow local connections via Unix socket
+      local   all   all                trust
+
+      # Allow IPv4 connections with password authentication
+      host    all   all   127.0.0.1/32   scram-sha-256
+
+      # Allow IPv6 connections with password authentication
+      host    all   all   ::1/128        scram-sha-256
+    '';
   };
 }
