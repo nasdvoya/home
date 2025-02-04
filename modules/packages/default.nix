536ce3541b5185932fc06846154a18cc4996cff4
sasha <nasdvoya@proton.me>
2025-02-04 00:48:27 +0000

    Rework

--- modules/packages/default.nix
+++ modules/packages/default.nix
@@ -1,14 +1,12 @@
 {...}: {
   imports = [
     ./starship.nix
-    ./alacritty.nix
     ./zellij.nix
     ./bash.nix
     ./lazygit.nix
     ./wezterm.nix
     ./atuin.nix
     ./yazi.nix
-    ./vscode.nix
     ./gpg.nix
   ];
 }
