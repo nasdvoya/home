718e2a1cc274a997d0981663ef723b1ea72e6b63
sasha <nasdvoya@proton.me>
2025-03-24 23:32:06 +0000

    Sync

--- modules/dotfiles/nvim/lua/config/plugins/colorscheme.lua
+++ modules/dotfiles/nvim/lua/config/plugins/colorscheme.lua
@@ -1,23 +1,11 @@
 return {
   {
-    "e-q/okcolors.nvim",
-    name = "okcolors",
-
+    "Skardyy/makurai-nvim",
     config = function()
-      vim.cmd.colorscheme("okcolors-smooth")
-      -- Set transparency
-      -- vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
-      -- vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" })
-      -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
-      -- vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "NONE" })
-      -- vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "NONE" })
-      -- vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "NONE", fg = "NONE" })
-      -- vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "NONE" })
-      -- vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "NONE", fg = "NONE" })
-      -- vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = "NONE" })
-      -- vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { bg = "NONE", fg = "NONE" })
-      -- vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = "NONE" })
-      -- vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { bg = "NONE", fg = "NONE" })
+      require("makurai").setup({
+        transparent = true,
+      })
+      vim.cmd.colorscheme("makurai")
     end,
   },
 }
